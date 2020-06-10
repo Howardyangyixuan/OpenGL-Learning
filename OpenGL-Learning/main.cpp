8419a6541c44b84e5b811edea1afeeec36403999
howardyangyixuan <howardyangyixuan@gmail.com>
2020-06-10 13:41:29 +0800

    Initial Commit

---
+++ OpenGL-Learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL-Learning
+//
+//  Created by 杨义轩 on 2020/6/10.
+//  Copyright © 2020 杨义轩. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
