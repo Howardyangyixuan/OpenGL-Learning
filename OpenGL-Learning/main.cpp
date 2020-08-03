1d0af5a8eafff020b25ba669a0ce375b658cca02
howardyangyixuan <howardyangyixuan@gmail.com>
2020-08-03 16:05:39 +0800

    每个顶点赋予颜色，通过VAO渲染得到彩色三角形（片段着色器片段插值）

--- OpenGL-Learning/main.cpp
+++ OpenGL-Learning/main.cpp
@@ -1,57 +1,3 @@
-//#define GLEW_STATIC
-//#include <GL/glew.h>
-//#include <GLFW/glfw3.h>
-//
-//
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//    //glewInit();
-//
-//    //glewIsSupported("GL_VERSION_2_0");
-//
-//
-//    glfwInit();
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-//    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-//        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-//    GLFWwindow* window = glfwCreateWindow(200, 200, "m30", nullptr, nullptr);
-//    if (window == nullptr)
-//    {
-//        cout << "Failed to create GLFW window" << endl;
-//        glfwTerminate();
-//        return -1;
-//    }
-//    glfwMakeContextCurrent(window);
-//
-//    int width, height;
-//    glfwGetFramebufferSize(window, &width, &height);
-//
-//
-//    glewInit();
-//    glViewport(0, 0, width, height);
-//    //glViewport(0, 0, width, height);
-//    GLuint buffer[1];
-//    glGenBuffers(1, buffer);
-//
-//    glGetString(0);
-//
-//
-//
-//    while (!glfwWindowShouldClose(window))
-//    {
-//        //cout << "m30" << endl;
-//        glfwPollEvents();
-//        glfwSwapBuffers(window);
-//    }
-//    glfwTerminate();
-//    return 0;
-//}
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <math.h>
@@ -63,34 +9,21 @@ const unsigned int SCR_HEIGHT = 600;
 using namespace std;
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
-//const char *vertexShaderSource = "#version 330 core\n"
-//    "layout (location = 0) in vec3 aPos;\n"
-//    "void main()\n"
-//    "{\n"
-//    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-//    "}\0";
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
-//    "out vec4 vertexColor;"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos,1.0);\n"
-//    "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+    "   ourColor = aColor;\n"
     "}\0";
-//const char *fragmentShaderSource = "#version 330 core\n"
-//    "out vec4 FragColor;\n"
-//    "void main()\n"
-//    "{\n"
-//    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-//    "}\n\0";
 const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
-//"in vec4 vertexColor;\n"
-"uniform vec4 ourColor;\n"
+    "in vec3 ourColor;\n"
     "void main()\n"
     "{\n"
-//"FragColor = vertexColor;\n"
-"FragColor = ourColor;\n"
+    "FragColor = vec4(ourColor,1.0);\n"
     "}\n\0";
 int main()
 {
@@ -155,27 +88,9 @@ int main()
     glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
     std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
     float vertices[]={
-    //        -0.5f, -0.5f, 0.0f,
-    //        0.5f, -0.5f, 0.0f,
-    //        0.0f,  0.5f, 0.0f,
-    //      //有冗余
-    //      // 第一个三角形
-    //       0.5f, 0.5f, 0.0f,   // 右上角
-    //       0.5f, -0.5f, 0.0f,  // 右下角
-    //       -0.5f, 0.5f, 0.0f,  // 左上角
-    //       // 第二个三角形
-    //       0.5f, -0.5f, 0.0f,  // 右下角
-    //       -0.5f, -0.5f, 0.0f, // 左下角
-    //       -0.5f, 0.5f, 0.0f   // 左上角
-            //使用索引，不输入重复的点
-        0.5f, 0.5f, 0.0f,   // 右上角
-        0.5f, -0.5f, 0.0f,  // 右下角
-        -0.5f, -0.5f, 0.0f, // 左下角
-        -0.5f, 0.5f, 0.0f   // 左上角
-    };
-    unsigned int indices[] = { // 注意索引从0开始!
-        0, 1, 3, // 第一个三角形
-        1, 2, 3  // 第二个三角形
+        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
+           -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
+            0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
     };
     //创建顶点缓冲对象
     unsigned int VBO;
@@ -189,15 +104,17 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     // 3. 设置顶点属性指针
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+    
     //创建EBO
     unsigned int EBO;
     glGenBuffers(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+//    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     //
-    int n =0;
     while(!glfwWindowShouldClose(window)){
         processInput(window);
         //绘制三角形
@@ -205,23 +122,10 @@ int main()
         glClearColor(0.2f,0.3f,0.3f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         //
-        float timeValue = glfwGetTime();
-        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUseProgram(shaderProgram);
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         //
-//        glUseProgram(shaderProgram);
+        glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        
-//        glDrawArrays(GL_TRIANGLES, 0, 3);
-//        glDrawArrays(GL_TRIANGLES, 3, 6);
-//        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        //切换：填充/线性
-//        n++;
-//        if(n%100>50)glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-//        else glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         //解绑
         glBindVertexArray(0);
         //绘制颜色
