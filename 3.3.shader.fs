e11b9fd067f72b3e011c66a8e39735982f249215
howardyangyixuan <howardyangyixuan@gmail.com>
2020-08-04 17:49:01 +0800

    引入stb_image.h处理图像，并完成纹理的处理过程 1. 创建纹理 2. 设置纹理坐标 3. 绑定纹理 4. 设置顶点属性 5. 配置纹理（过滤、环绕） 6. 读取纹理，绘制

--- 3.3.shader.fs
+++ 3.3.shader.fs
@@ -2,8 +2,9 @@
 out vec4 FragColor;
 
 in vec3 ourColor;
-
+in vec2 ourTextureCoord;
+uniform sampler2D ourTexture;
 void main()
 {
-    FragColor = vec4(ourColor, 1.0f);
+    FragColor = texture(ourTexture,ourTextureCoord);
 }
--- OpenGL-Learning/main.cpp
+++ OpenGL-Learning/main.cpp
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <iostream>
 #include "Shader.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 using namespace std;
@@ -19,7 +21,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,GL_TRUE);
     cout<<"完成glfw实例化"<<endl;
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello OpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hello OpenGL", NULL, NULL);
     if(window==NULL){
 //        cout<<window<<endl;
         cout<<"创建glfw窗口失败"<<endl;
@@ -48,11 +50,38 @@ int main()
     int nrAttributes;
     glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
     std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
-    float vertices[]={
-        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
-           -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
-            0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
+    float vertices[] = {
+    //     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
     };
+    unsigned int indices[] = { // 注意索引从0开始!
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+    };
+    //创建纹理
+    unsigned int texture;
+    glad_glGenTextures(1,&texture);
+    glad_glBindTexture(GL_TEXTURE_2D,texture);
+    //设置环绕和过滤
+    glad_glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+    glad_glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+    glad_glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    glad_glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    //加载纹理
+    int width, height, nrChannels;
+      unsigned char *data = stbi_load("../wall.jpg", &width, &height, &nrChannels, 0);
+    if(data){
+        glad_glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,data);
+        glad_glGenerateMipmap(GL_TEXTURE_2D);
+    }else{
+        std::cout<<"加载纹理失败"<<std::endl;
+    }
+    stbi_image_free(data);
+    
+    
     //创建顶点缓冲对象
     unsigned int VBO;
     glGenBuffers(1,&VBO);
@@ -65,16 +94,18 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     // 3. 设置顶点属性指针
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(2*3*sizeof(float)));
+    glEnableVertexAttribArray(2);
     
     //创建EBO
     unsigned int EBO;
     glGenBuffers(1, &EBO);
-//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-//    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     //
     while(!glfwWindowShouldClose(window)){
         processInput(window);
@@ -86,7 +117,9 @@ int main()
         //
         ourShader.use();
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glad_glBindTexture(GL_TEXTURE_2D,texture);
+//        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glad_glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
         //解绑
         glBindVertexArray(0);
         //绘制颜色
