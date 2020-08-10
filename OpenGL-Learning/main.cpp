ccbd74f83ebe94866ae858c746995a878aa44c80
howardyangyixuan <howardyangyixuan@gmail.com>
2020-08-10 22:37:23 +0800

    增加物体数量

--- OpenGL-Learning/main.cpp
+++ OpenGL-Learning/main.cpp
@@ -93,6 +93,19 @@ int main()
         -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
+    //10个位移向量
+    glm::vec3 cubePositions[] = {
+      glm::vec3( 0.0f,  0.0f,  0.0f),
+      glm::vec3( 2.0f,  5.0f, -15.0f),
+      glm::vec3(-1.5f, -2.2f, -2.5f),
+      glm::vec3(-3.8f, -2.0f, -12.3f),
+      glm::vec3( 2.4f, -0.4f, -3.5f),
+      glm::vec3(-1.7f,  3.0f, -7.5f),
+      glm::vec3( 1.3f, -2.0f, -2.5f),
+      glm::vec3( 1.5f,  2.0f, -2.5f),
+      glm::vec3( 1.5f,  0.2f, -1.5f),
+      glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
     //创建纹理
     unsigned int texture1,texture2;
     glad_glGenTextures(1,&texture1);
@@ -177,20 +190,23 @@ int main()
         // 译注：下面就是矩阵初始化的一个例子，如果使用的是0.9.9及以上版本
         // glm::mat4 trans;
         // 这行代码就需要改为:
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model,(float)glfwGetTime()*glm::radians(-55.0f),glm::vec3(0.5f,1.0f,0.0f));
         glm::mat4 view = glm::mat4(1.0f);
         view = glm::translate(view, glm::vec3(0.0f,0.0f,-3.0f));
         glm::mat4 projection = glm::mat4(1.0f);
         projection = glm::perspective(glm::radians(45.0f),float(SCR_WIDTH / SCR_HEIGHT), 0.1f, 100.0f);
-        ourShader.setMat4("model", model);
         ourShader.setMat4("view", view);
         ourShader.setMat4("projection", projection);
         ourShader.setFloat("mixPercent", mixPercent);
         
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-        
+        for(int i=0;i<10;i++){
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            model = glm::rotate(model,20*i+(float)glfwGetTime()*glm::radians(-55.0f),glm::vec3(0.5f,1.0f,0.0f));
+            ourShader.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
         //解绑
         glBindVertexArray(0);
         //绘制颜色
