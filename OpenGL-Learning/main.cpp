8bf57fdaa70a3dfeb946470e76fe39d461d11f92
howardyangyixuan <howardyangyixuan@gmail.com>
2020-08-10 22:47:11 +0800

    坐标系统小练习 三合一

--- OpenGL-Learning/main.cpp
+++ OpenGL-Learning/main.cpp
@@ -191,7 +191,7 @@ int main()
         // glm::mat4 trans;
         // 这行代码就需要改为:
         glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f,0.0f,-3.0f));
+        view = glm::translate(view, glm::vec3(1.0f,1.0f,-10.0f));
         glm::mat4 projection = glm::mat4(1.0f);
         projection = glm::perspective(glm::radians(45.0f),float(SCR_WIDTH / SCR_HEIGHT), 0.1f, 100.0f);
         ourShader.setMat4("view", view);
@@ -202,7 +202,9 @@ int main()
         for(int i=0;i<10;i++){
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
-            model = glm::rotate(model,20*i+(float)glfwGetTime()*glm::radians(-55.0f),glm::vec3(0.5f,1.0f,0.0f));
+            model = glm::rotate(model,(float)20*i,glm::vec3(0.5f,1.0f,0.0f));
+            if(i%3==0)
+            model = glm::rotate(model,(float)glfwGetTime()*glm::radians(-55.0f),glm::vec3(0.5f,1.0f,0.0f));
             ourShader.setMat4("model", model);
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
